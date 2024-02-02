1cc08607e451c2f2a6cec83c6713655a8b6a25e5
Camilla Cummins <2796631C@student.gla.ac.uk>
2024-02-02 18:22:42 +0000

    chapter 3 test commit

--- rango/views.py
+++ rango/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from django.urls import reverse
+#c4- from django.urls import reverse
 
 def index(request):
-    context_dict = {
-        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
-        'about_url' : reverse('about'),
-    }
-    return render(request, 'rango/index.html', context=context_dict)
-
-    #return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner!, <a href='/rango/about/'>About</a>")
 
 def about(request):
-    context_dict = {
-        'boldmessage': 'This tutorial has been put together by Camilla.',
-        'index_url' : reverse('index')
-}
-    return render(request, 'rango/about.html', context=context_dict)
+    return HttpResponse("Rango says here is the about page. , <a href='/rango/'>Index</a>")
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -22,7 +22,9 @@ from rango import views
 
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    #path('about/', views.about, name = 'about'), 
+    #delete two above urls for chapter 4   
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
