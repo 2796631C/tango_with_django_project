00666f8f6e41c8407bdc5879860d9e3bbec7c2c3
Camilla Cummins <2796631C@student.gla.ac.uk>
2024-02-07 18:00:53 +0000

    chapter 6

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,18 @@ app_name = 'rango'
 urlpatterns = [
 path('', views.index, name='index'),
 path('about/', views.about, name = 'about'),
+path('category/<slug:category_name_slug>/',
+    views.show_category, name='show_category'),
 ]
+
+#<h2>Most Liked Categories</h2>
+
+#        <h2>Most Viewed Pages</h2>
+#        <div>
+#        {% if pages %}
+#            <ul>
+#                {% for page in pages %}
+#                <li>
+#                    <a href="/rango/category/category">
+#
+#        </div>
